0d3e8e8382ed42de8689c717634522f9aa1479ca
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-08-20 14:09:40 -0400

    created Articles model

--- djangoblog/articles/urls.py
+++ djangoblog/articles/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('about/', views.about),
-    path('', views.home)
+    path('', views.article_list)
 ]
